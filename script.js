431cb5c51dea21ef36996c8475fd5293a58482d1
AlMursalat <127673328+AlMursalat@users.noreply.github.com>
2023-03-12 22:24:43 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function klik(){
-    document.getElementById("tgl").innerHTML="SITI AISYAH AZ ZAHRA"
-    document.getElementById("gambar").src="https://cdn.pixabay.com/photo/2013/07/12/19/02/happy-birthday-154242_1280.png"
-    document.getElementById("tulisan").innerHTML="“Happy birthday, Bestiii! Terima kasih telah menjadi teman yang terbaik dari yang terbaik. Semoga diberikan umur panjang dan sehat selalu, semoga juga ko lulus tes di universitas yang ko suka🤗🎂🙈🔥”"
-    document.getElementById("tombol").innerHTML="13-3-2023"
-}
